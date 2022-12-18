6c2445bc7b4e857477d334221d562c2a891cc5c6
jiaozhenpeng <50771779+jiaozhenpeng@users.noreply.github.com>
2022-12-18 12:44:54 +0800

    20221218

---
+++ test_case/test_hu_a/test_changes_in_securities/contrast_B.py
@@ -0,0 +1,88 @@
+import unittest
+
+from config import PathConfig
+from database.oracle_database import OracleDatabase
+from log.logger import logger
+from public_method.base_action import BaseAction
+from public_method.excel_operation import ExcelOperation
+
+
+class ContrastRestrictedShares(unittest.TestCase):
+    """
+    沪A\各种证券变动\00B
+    """
+    yaml = BaseAction().read_yaml(path=PathConfig().hu_a())['ChangesInSecurities']['B']
+    ignore = BaseAction().read_yaml(path=PathConfig().table_ignore())
+
+    def test_restricted_shares(self):
+        """
+        沪A\各种证券变动\00B
+        :return:
+        """
+        logger().info('-------------------------------')
+        logger().info('开始执行：沪A\各种证券变动\00B 对比数据')
+        excel_path = self.yaml['excelPath']
+        excel = ExcelOperation(excel_path)
+        oracle = OracleDatabase()
+        begintime = oracle.get_last_update()
+        endtime = begintime[0:8] + '235959'
+        base = BaseAction()
+        year = base.get_today_date()[:4]
+
+        # 查询sql
+        stklistextend_sql = " select * from   stklistextend where exchid='0' and offerregid in('A117212000'," \
+                            "'A117252000' ) and stkid in('601028','601066','601068','601369','601375')"
+        stklist_sql = "select * from  stklist where exchid='0' and offerregid in('A117212000','A117252000' ) " \
+                      "and stkid in('601028','601066','601068','601369','601375')"
+        stklisthis_sql = " select * from stklist{} where occurtime={} and stkid in ('601028','601066','601068'," \
+                         "'601369','601375') ".format(year,begintime)
+        stklistextendhis_sql = " select * from stklistextend{} where occurtime={} and stkid in ('601028','601066','601068'," \
+                         "'601369','601375') ".format(year,begintime)
+        tradinglog_sql = "select * from tradinglog{} where reckoningtime>={} and reckoningtime<={} and exchid= '0'  " \
+                         "and stkid in ('601028','601066','601068','601369','601375') ".format(year, begintime, endtime)
+        # 获取数据库数据并排序
+
+        stklistextend_database = BaseAction().stklistextend_sort(oracle.dict_data(stklistextend_sql))
+        stklistextendhis_database = BaseAction().stklistextend_sort(oracle.dict_data(stklistextendhis_sql))
+        stklist_database = BaseAction().stklist_sort(oracle.dict_data(stklist_sql))
+        stklisthis_database = BaseAction().stklist_sort(oracle.dict_data(stklisthis_sql))
+        tradinglog_database = base.tradinglog_sort(oracle.dict_data(tradinglog_sql))
+
+
+
+        # 获取excel数据并排序
+        stklistextend_excel = BaseAction().stklistextend_sort(excel.read_excel('stklistextend'))
+        stklist_excel = BaseAction().stklist_sort(excel.read_excel('stklist'))
+        tradinglog_excel = base.tradinglog_sort(excel.read_excel('tradinglog'))
+        stklistextendhis_excel = BaseAction().stklistextend_sort(excel.read_excel('stklistextend2022'))
+        stklisthis_excel = BaseAction().stklist_sort(excel.read_excel('stklist2022'))
+
+
+
+        # 可以忽略的字段
+        stklistextendhis_ignore= self.ignore['stklisthis']
+        stklisthis_ignore = self.ignore['stklistextendhis']
+        tradinglog_ignore = self.ignore['tradinglog']
+
+        # 对比数据
+
+        stklist_result = BaseAction().compare_dict(stklist_database,stklist_excel,'stklist')
+        stklistextend_result = BaseAction().compare_dict(stklistextend_database,stklistextend_excel,'stklistextend')
+        stklisthis_result = BaseAction().compare_dict(stklisthis_database,stklisthis_excel,'stklist2022',*stklisthis_ignore)
+        stklistextendhis_result = BaseAction().compare_dict(stklistextendhis_database,stklistextendhis_excel,
+                                                            'stklistextend2022',*stklistextendhis_ignore)
+        tradinglog_result = base.compare_dict(tradinglog_database, tradinglog_excel, 'tradinglog', *tradinglog_ignore)
+
+        final_result = stklist_result + stklistextend_result + stklisthis_result + stklistextendhis_result + tradinglog_result
+
+        # 断言
+        if not final_result :
+            logger().info('沪A\各种证券变动\00B 对比数据无异常')
+            assert True
+        else:
+            logger().error('沪A\各种证券变动\00B 对比数据异常')
+            assert False, final_result
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test_case/test_hu_quan/test_reserve_exercise/contrast_reserve_exercise.py
+++ test_case/test_hu_quan/test_reserve_exercise/contrast_reserve_exercise.py
@@ -12,6 +12,8 @@ class ContrastReserveExercise(unittest.TestCase):
     沪权\备兑后行权
     """
     yaml = BaseAction().read_yaml(path=PathConfig().hu_quan())['ReserveExercise']
+    ignore = BaseAction().read_yaml(path=PathConfig().table_ignore())
+
 
     def test_reserve_exercise(self):
         """
@@ -28,40 +30,51 @@ class ContrastReserveExercise(unittest.TestCase):
         base = BaseAction()
         year = base.get_today_date()[:4]
         # 查询SQL
-        futureposition_sql = "select * from futureposition WHERE regid='A117212005'  and " \
-                             "stkid ='10003527' and exchid = 'X'"
-        stkoptionsettlement_sql = "select * from stkoptionsettlement  where reckoningtime>={} and reckoningtime<={} " \
-                                  "and exchid='X' and regid='A117212005' and stkid ='10003527' ".format(begintime,
-                                                                                                        endtime)
         futuretradinglog_sql = "select * from futuretradinglog{}  where reckoningtime>={} and reckoningtime<={} and " \
-                               "exchid='X' and regid='A117212005' and stkid ='10003527'".format(year, begintime,
-                                                                                                endtime)
-        futurepositiondetail_sql = "select * from futurepositiondetail WHERE regid='A117212005'  and occurtime={} and " \
-                                   "stkid ='10003527' and exchid = 'X'".format(begintime)
-        # 数据库数据
+                               "exchid='X' and regid='A117212005' and stkid in('10003527')".format(year, begintime,
+                                                                                                   endtime)
+        futurepositionhis_sql = "select * from futureposition{} WHERE regid='A117212005'  and occurtime={} and stkid " \
+                                "in(10003527)".format(year, begintime)
+        futurepositiondetailhis_sql = "select * from futurepositiondetail{} WHERE regid='A117212005'  and occurtime={} " \
+                                      "and stkid in(10003527)".format(year, begintime)
+        futureposition_sql = "select * from futureposition WHERE regid='A117212005'   and stkid  in(10003527) "
+        futurepositiondetail_sql = "select * from futurepositiondetail WHERE regid='A117212005'   and stkid" \
+                                   " in(10003527) "
+
+        # 获取数据库数据
+        futurepositiondetailhis_database = base.futurepositiondetail_sort(oracle.dict_data(futurepositiondetailhis_sql))
         futuretradinglog_database = base.futuretradinglog_sort(oracle.dict_data(futuretradinglog_sql))
-        futurepositiondetail_database = base.futurepositiondetail_sort(oracle.dict_data(futurepositiondetail_sql))
-        stkoptionsettlement_database = base.stkoptionsettlement_sort(oracle.dict_data(stkoptionsettlement_sql))
+        futurepositionhis_database = base.futureposition_sort(oracle.dict_data(futurepositionhis_sql))
         futureposition_database = base.futureposition_sort(oracle.dict_data(futureposition_sql))
-        # excel数据
+        futurepositiondetail_database = base.futurepositiondetail_sort(oracle.dict_data(futurepositiondetail_sql))
+        # excel 数据
         futuretradinglog_excel = base.futuretradinglog_sort(excel.read_excel('futuretradinglog'))
-        futurepositiondetail_excel = base.futurepositiondetail_sort(excel.read_excel('futurepositiondetail'))
-        stkoptionsettlement_excel = base.stkoptionsettlement_sort(excel.read_excel('stkoptionsettlement'))
+        futurepositionhis_excel = base.futureposition_sort(excel.read_excel('futureposition2022'))
+        futurepositiondetailhis_excel = base.futurepositiondetail_sort(excel.read_excel('futurepositiondetail2022'))
         futureposition_excel = base.futureposition_sort(excel.read_excel('futureposition'))
+        futurepositiondetail_excel = base.futurepositiondetail_sort(excel.read_excel('futurepositiondetail'))
+
         # 忽略字段
-        futuretradinglog_ignore = ()
-        futurepositiondetail_ignore = ()
-        stkoptionsettlement_ignore = ()
-        futureposition_ignore = ()
-        # 对比数据
+        futuretradinglog_ignore = self.ignore['futuretradinglog1']
+        futurepositionhis_ignore = self.ignore['futurepositionhis']
+        futurepositiondetail_ignore = self.ignore['futurepositiondetail1']
+        futurepositiondetailhis_ignore = self.ignore['futurepositiondetailhis1']
+
+        # 对比结果
         futuretradinglog_result = base.compare_dict(futuretradinglog_database, futuretradinglog_excel,
-                                                    'futuretradinglog')
-        futurepositiondetail_result = base.compare_dict(futurepositiondetail_database, futurepositiondetail_excel,
-                                                        'futurepositiondetail')
-        stkoptionsettlement_result = base.compare_dict(stkoptionsettlement_database, stkoptionsettlement_excel,
-                                                       'stkoptionsettlement')
+                                                    'futuretradinglog', *futuretradinglog_ignore)
+        futurepositionhis_result = base.compare_dict(futurepositionhis_database, futurepositionhis_excel,
+                                                     'futureposition2022', *futurepositionhis_ignore)
+        futurepositiondetailhis_result = base.compare_dict(futurepositiondetailhis_database,
+                                                           futurepositiondetailhis_excel,
+                                                           'futurepositiondetail2022', *futurepositiondetailhis_ignore)
         futureposition_result = base.compare_dict(futureposition_database, futureposition_excel, 'futureposition')
-        final_result = futuretradinglog_result + futurepositiondetail_result + stkoptionsettlement_result + futureposition_result
+        futurepositiondetail_result = base.compare_dict(futurepositiondetail_database, futurepositiondetail_excel,
+                                                        'futurepositiondetail', *futurepositiondetail_ignore)
+        # 断言
+        final_result = futuretradinglog_result + futureposition_result + futurepositiondetail_result \
+                       + futurepositionhis_result + futurepositiondetailhis_result
+
         if not final_result:
             logger().info('沪权\备兑后行权 对比数据无异常')
             assert True
